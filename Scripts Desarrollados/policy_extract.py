55aa6c21093fc5584dbe9cd5bf3c19f2f9b6c14c
JaimeRamirez-coder <58531730+JaimeRamirez-coder@users.noreply.github.com>
2022-06-05 22:57:40 -0500

    Update policy_extract.py

--- Scripts Desarrollados/policy_extract.py
+++ Scripts Desarrollados/policy_extract.py
@@ -80,8 +80,5 @@ def policy_extract(url):
 		return text
 
 
-# Copyright © 2021, 2022 Jaime Patricio Ramírez Ramírez.
+# Copyright © 2021, 2022 Jaime Ramírez.
 # This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation
-
-
-
